30ec04a297065718a6219f86eead06724a5728d8
ricco-arisdy <114916298+ricco-arisdy@users.noreply.github.com>
2024-06-24 08:00:06 +0700

    add file provider & class AuthProvider

---
+++ lib/provider/AuthProvider.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AuthProvider with ChangeNotifier {
+  int? _userId;
+
+  int? get userId => _userId;
+
+  void setUserId(int userId) {
+    _userId = userId;
+    notifyListeners();
+  }
+
+  void clearUser() {
+    _userId = null;
+    notifyListeners();
+  }
+}
