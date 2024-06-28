1011618315aff87fe849ff2d24b548c5aafad900
ricco-arisdy <114916298+ricco-arisdy@users.noreply.github.com>
2024-06-28 07:29:18 +0700

    dekorasi halaman loginPage

--- lib/screen/login_screen.dart
+++ lib/screen/login_screen.dart
@@ -18,6 +18,9 @@ class _LoginPageState extends State<LoginPage> {
   final _formKey = GlobalKey<FormState>();
   String errorMessage = '';
 
+  bool isRegisterTextButtonHovered = false;
+  bool isLoginTextButtonHovered = false;
+
   @override
   void initState() {
     _loginController = LoginController();
@@ -27,10 +30,38 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Tani Jaya'),
-        centerTitle: true,
-        backgroundColor: Color.fromRGBO(39, 246, 56, 0.824),
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(210),
+        child: ClipPath(
+          clipper: WaveClipper(),
+          child: AppBar(
+            title: const Text(
+              'Tani Jaya',
+              style: TextStyle(
+                color: Colors.white,
+                fontFamily: 'Montserrat',
+                fontSize: 24.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            centerTitle: true,
+            backgroundColor: Color.fromRGBO(39, 246, 56, 0.824),
+            elevation: 0,
+            flexibleSpace: Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    Color.fromRGBO(39, 246, 56, 0.824),
+                    Color.fromRGBO(29, 201, 52, 0.824),
+                    Color.fromRGBO(19, 157, 47, 0.824),
+                  ],
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                ),
+              ),
+            ),
+          ),
+        ),
       ),
       body: Center(
         child: SingleChildScrollView(
@@ -40,27 +71,98 @@ class _LoginPageState extends State<LoginPage> {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                CircleAvatar(
-                  radius: 100,
-                  backgroundImage: AssetImage('assets/natural_product.png'),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    GestureDetector(
+                      onTap: () {
+                        // Handle Login button tap
+                      },
+                      child: MouseRegion(
+                        onEnter: (_) {
+                          setState(() {
+                            isLoginTextButtonHovered = true;
+                          });
+                        },
+                        onExit: (_) {
+                          setState(() {
+                            isLoginTextButtonHovered = false;
+                          });
+                        },
+                        child: Text(
+                          'Login',
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                            decoration: isLoginTextButtonHovered
+                                ? TextDecoration.underline
+                                : TextDecoration.none,
+                            color: isLoginTextButtonHovered
+                                ? Color.fromARGB(255, 92, 227, 96)
+                                : Colors.black,
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(width: 100),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => RegisterPage()),
+                        );
+                      },
+                      child: MouseRegion(
+                        onEnter: (_) {
+                          setState(() {
+                            isRegisterTextButtonHovered = true;
+                          });
+                        },
+                        onExit: (_) {
+                          setState(() {
+                            isRegisterTextButtonHovered = false;
+                          });
+                        },
+                        child: Text(
+                          'Register',
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                            decoration: isRegisterTextButtonHovered
+                                ? TextDecoration.underline
+                                : TextDecoration.none,
+                            color: isRegisterTextButtonHovered
+                                ? Color.fromARGB(255, 92, 227, 96)
+                                : Colors.black,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
-                const SizedBox(height: 20),
+                const SizedBox(height: 60),
                 Text(
                   'Welcome Back!',
-                  style: Theme.of(context).textTheme.headlineMedium,
+                  style: Theme.of(context).textTheme.headline6,
                 ),
                 const SizedBox(height: 20),
                 TextFormField(
                   controller: emailController,
                   decoration: InputDecoration(
                     labelText: 'Email',
-                    border: OutlineInputBorder(),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12.0),
+                    ),
+                    prefixIcon: Icon(Icons.email),
+                    filled: true,
+                    fillColor: Colors.grey[200],
                   ),
                   validator: (value) {
                     if (value == null || value.isEmpty) {
                       return 'Please enter your email';
                     }
-                    if (!RegExp(r'^[^@]+@[^@]+\.[^@]+').hasMatch(value)) {
+                    if (!RegExp(r'^[^@]+@[^@]+\.[^@]+').hasMatch(value!)) {
                       return 'Please enter a valid email';
                     }
                     return null;
@@ -72,7 +174,12 @@ class _LoginPageState extends State<LoginPage> {
                   obscureText: true,
                   decoration: InputDecoration(
                     labelText: 'Password',
-                    border: OutlineInputBorder(),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12.0),
+                    ),
+                    prefixIcon: Icon(Icons.lock),
+                    filled: true,
+                    fillColor: Colors.grey[200],
                   ),
                   validator: (value) {
                     if (value == null || value.isEmpty) {
@@ -84,38 +191,34 @@ class _LoginPageState extends State<LoginPage> {
                 const SizedBox(height: 20),
                 ElevatedButton(
                   onPressed: () async {
-                    if (_formKey.currentState?.validate() ?? false) {
+                    if (_formKey.currentState!.validate()) {
                       String email = emailController.text;
                       String password = passwordController.text;
                       int? userId = await _loginController.login(
-                          context, email, password);
+                        context,
+                        email,
+                        password,
+                      );
                       if (userId != null) {
                         Navigator.pushReplacement(
                           context,
                           MaterialPageRoute(
-                              builder: (context) => HomeView(userId: userId)),
+                            builder: (context) => HomeView(userId: userId),
+                          ),
                         );
                       }
                     }
                   },
                   child: const Text('Login'),
                   style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color.fromARGB(255, 92, 227, 96),
                     padding: const EdgeInsets.symmetric(
-                      horizontal: 50,
+                      horizontal: 150,
                       vertical: 15,
                     ),
                   ),
                 ),
                 const SizedBox(height: 20),
-                TextButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => RegisterPage()),
-                    );
-                  },
-                  child: const Text('Don’t have an account? Register here'),
-                ),
                 if (errorMessage.isNotEmpty)
                   Padding(
                     padding: const EdgeInsets.only(top: 20),
@@ -130,5 +233,30 @@ class _LoginPageState extends State<LoginPage> {
         ),
       ),
     );
+  }
+}
+
+class WaveClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    var path = Path();
+    path.lineTo(-0, size.height - 50);
+    var firstControlPoint = Offset(size.width / 4, size.height);
+    var firstEndPoint = Offset(size.width / 2, size.height - 50);
+    var secondControlPoint = Offset(size.width * 3 / 4, size.height - 100);
+    var secondEndPoint = Offset(size.width, size.height - 10);
+
+    path.quadraticBezierTo(firstControlPoint.dx, firstControlPoint.dy,
+        firstEndPoint.dx, firstEndPoint.dy);
+    path.quadraticBezierTo(secondControlPoint.dx, secondControlPoint.dy,
+        secondEndPoint.dx, secondEndPoint.dy);
+    path.lineTo(size.width, 0);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) {
+    return false;
   }
 }
