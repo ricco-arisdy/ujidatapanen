408302d7a6145a665d12f74a23bed40ca2e99798
ricco-arisdy <114916298+ricco-arisdy@users.noreply.github.com>
2024-07-03 01:45:16 +0700

    membuat viewpanenbyloadingservice

---
+++ lib/service/ViewPanenServicebyLoading.dart
@@ -0,0 +1,19 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:ujidatapanen/model/panen.dart';
+
+class ViewPanenServiceLoading {
+  static const String apiUrl =
+      'http://192.168.0.197/api_pam/get_panenbyloading.php';
+
+  Future<List<Panen>> fetchPanen(int idLoading) async {
+    final response = await http.get(Uri.parse('$apiUrl?id_loading=$idLoading'));
+
+    if (response.statusCode == 200) {
+      List<dynamic> body = jsonDecode(response.body);
+      return body.map((dynamic item) => Panen.fromJson(item)).toList();
+    } else {
+      throw Exception('Failed to load panen data');
+    }
+  }
+}
